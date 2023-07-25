e00216ce90de9eb9e09402c1d778a46ea011d94a
lucas wanderosck <lucaskleinwanderosck@gmail.com>
2023-07-25 15:41:46 -0300

    configurando o ambiente de desenvolvimento

---
+++ src/Teste.tsx
@@ -0,0 +1,11 @@
+import styled from 'styled-components'
+
+const Button = styled.button`
+  background-color: black;
+`
+
+const Teste = () => {
+  return <Button>Clique aqui</Button>
+}
+
+export default Teste
